085a5425af27ab9f3b1b22b4dfb3371a545a636d
LeoMosiah <leonardomosiah@gmail.com>
2020-11-14 16:43:19 -0300

    add signIn feature

--- app/src/main/java/com/example/dory/AuthenticationActivity.java
+++ app/src/main/java/com/example/dory/AuthenticationActivity.java
@@ -2,13 +2,40 @@ package com.example.dory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class AuthenticationActivity extends AppCompatActivity {
+    private final FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private final String TAG = "AuthenticationActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
+    }
+
+    public void signIn(View view) {
+        EditText emailEditText = findViewById(R.id.editTextEmail);
+        EditText passwordEditText = findViewById(R.id.editTextPassword);
+        mAuth.signInWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString())
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        Log.d(TAG, "signInWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        startActivity(new Intent(this, MainActivity.class));
+                    } else {
+                        Log.w(TAG, "signInWithEmail:failure", task.getException());
+                        Toast.makeText(AuthenticationActivity.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/dory/MainActivity.java
+++ app/src/main/java/com/example/dory/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.dory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = "MainActivity";
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        SharedPreferences prefs = this.getSharedPreferences(
+                "com.example.dory", Context.MODE_PRIVATE);
+        String lastTimeLoggedIn = Calendar.getInstance().getTime().toString();
+        prefs.edit().putString("lastTimeLoggedIn", lastTimeLoggedIn).apply();
+        Log.d(TAG, lastTimeLoggedIn + "Saved with success");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FirebaseAuth mFirebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        SharedPreferences prefs = this.getSharedPreferences(
+                "com.example.dory", Context.MODE_PRIVATE);
+        String defaultLastTimeLoggedIn = Calendar.getInstance().getTime().toString();
+        String lastTimeLoggedIn = prefs.getString("lastTimeLoggedIn", defaultLastTimeLoggedIn);
+        Log.d(TAG, lastTimeLoggedIn);
         super.onCreate(savedInstanceState);
+        if (mFirebaseUser == null) {
+            startActivity(new Intent(this, AuthenticationActivity.class));
+            finish();
+            return;
+        }
         setContentView(R.layout.activity_main);
     }
 }
